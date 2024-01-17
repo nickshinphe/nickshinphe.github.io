acd8dbf549cc37d5b2538effd9d5dcf22363416a
Nick Shin <nick.shin@phenixrts.com>
2024-01-17 16:01:36 -0600

    original valid, trunc (50% invalid) invalid, intervals at 5secs, with video - mk:06a7

---
+++ TEST/PLI/06a7-android-h264/channel-viewer-with-encoded-video-stream.js
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2023 Phenix Real Time Solutions, Inc. Confidential and Proprietary. All Rights Reserved.
+ */
+/* global phenix */
+// Alternate ways to load the sdk directly:
+// import phenix from '@phenixrts/sdk';
+// import phenix from '../../dist/channels/min/channels';
+// import phenix from '../../src/sdk/channels';
+
+console.log('NIKNIK: android H264 - build 6a7 - original valid, trunc (50% of original) invalid, intervals @ 5secs - with video');
+
+// ------------------------------------------------------------
+const status = document.getElementById('PliStatus');
+let triggerPli = false;
+
+setInterval(() => {
+  triggerPli = true;
+  status.innerHTML = 'status: PLIs for each frame';
+}, 5000);
+
+// ------------------------------------------------------------
+function processVideo(track, frame) {
+  if (triggerPli) {
+    triggerPli = false;
+    status.innerHTML = 'status: GOOD';
+
+    return false;
+  }
+
+  return true;
+}
+
+// ------------------------------------------------------------
+function processAudio(track, frame) {
+  return true;
+}
+
+// ------------------------------------------------------------
+// The token capabilities must include 'encoded-insertable-streams' to
+// use "Encoded Transform" API
+const videoElement = document.getElementsByTagName('video')[0];
+const createChannelOptions = {
+  videoElement,
+  token: new URLSearchParams(location.search).get('token') || '',
+  encodedVideoStreamSink: processVideo,
+  encodedAudioStreamSink: processAudio
+};
+
+phenix.Channels.createChannel(createChannelOptions);
