582b53bf70697b973c57f499e18599b0674e1f27
Nick Shin <nick.shin@phenixrts.com>
2024-04-19 13:27:16 -0500

    gerrit - PS36 - interval - 36a1 mk1

---
+++ TEST/PLI/36a1-gerrit/channel-viewer-with-encoded-video-stream.js
@@ -0,0 +1,53 @@
+/**
+ * Copyright 2023 Phenix Real Time Solutions, Inc. Confidential and Proprietary. All Rights Reserved.
+ */
+/* global phenix */
+// Alternate ways to load the sdk directly:
+// import phenix from '@phenixrts/sdk';
+// import phenix from '../../dist/channels/min/channels';
+// import phenix from '../../src/sdk/channels';
+
+const version = '36a1';
+console.log(`NIKNIK: gerrit - build ${version} - PS36 - interval - mk1`);
+
+// ------------------------------------------------------------
+const status = document.getElementById('PliStatus');
+let triggerPli = false;
+
+status.innerHTML = `${version} status: GOOD`;
+setInterval(() => {
+  triggerPli = !triggerPli;
+
+  if (triggerPli) {
+    status.innerHTML = `${version} status: PLIs for each frame`;
+  } else {
+    status.innerHTML = `${version} status: GOOD`;
+  }
+}, 15000);
+
+// ------------------------------------------------------------
+function processVideo(track, frame) {
+  if (triggerPli) {
+    return false;
+  }
+
+  return true;
+}
+
+// ------------------------------------------------------------
+function processAudio(track, frame) {
+  return true;
+}
+
+// ------------------------------------------------------------
+// The token capabilities must include 'encoded-insertable-streams' to
+// use "Encoded Transform" API
+const videoElement = document.getElementsByTagName('video')[0];
+const createChannelOptions = {
+  videoElement,
+  token: new URLSearchParams(location.search).get('token') || '',
+  encodedVideoStreamSink: processVideo,
+  encodedAudioStreamSink: processAudio
+};
+
+phenix.Channels.createChannel(createChannelOptions);
